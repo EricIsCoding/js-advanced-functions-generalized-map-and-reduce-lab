70a0f410f0cfeceda1a25d6a6bf52af7c45a213b
Eric J Wilhite <EricJWilhite@gmail.com>
2020-05-30 16:50:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(array, callback) {
+    let result = []
+  
+    for (let i = 0; i < array.length; i++) {
+      let theElement = array[i]
+      result.push(callback(theElement))
+    }
+  
+    return result;
+  }
+  
+  function reduce(array, callback, start){
+    let result = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+  
+    for (; i < array.length; i++) {
+      result = callback(array[i], result)
+    }
+  
+    return result;
+  }
